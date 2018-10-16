cf3aa35463d28e215e2ecbe453e994ffa3e04879
unizalin <unizalin@gmail.com>
2018-10-16 22:28:05 +0800

    vue 中運用 jquery 跟 bootstrap

--- src/components/pages/Products.vue
+++ src/components/pages/Products.vue
@@ -1,7 +1,8 @@
 <template>
   <div>
     <div class="text-right mt-4">
-      <button class="btn btn-primary">建立新的產品</button>
+    <button type="button" class="btn btn-primary" @click="openModel">
+        建立新的產品</button>
     </div>
     <table class="table mt-4">
       <thead>
@@ -34,11 +35,33 @@
         </tr>
       </tbody>
     </table>
+
+    <!-- Modal -->
+    <div class="modal fade" id="productModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="modal-body">
+            ...
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+            <button type="button" class="btn btn-primary">Save changes</button>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 <script>
-export default {
+  import $ from 'jquery';
 
+export default {
   data() {
     return {
       products: [],
@@ -53,6 +76,9 @@ export default {
         console.log(response.data);
         vm.products = response.data.products;
       });
+    },
+    openModel(){
+      $('#productModal').modal('show')
     },
   },
   created() {
