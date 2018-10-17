b0d8a567599f453ad48fb79cfdb64a4aafcaf3aa
UNIZA <unizalin@gmail.com>
2018-10-17 16:54:53 +0800

    錯誤得訊息回饋

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <Navbar/>
+    <Alert></Alert>
     <div class="container-fluid">
       <div class="row">
         <Sidebar></Sidebar>
@@ -14,10 +15,12 @@
 <script>
 import Sidebar from "./Sidebar";
 import Navbar from "./Navbar";
+import Alert from "./AlertMessage";
 export default {
   components: {
     Sidebar,
-    Navbar
+    Navbar,
+    Alert
   }
 };
 </script>
